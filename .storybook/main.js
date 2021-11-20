1977499d9f985f3433855144b2c6f860b35cfeed
rafalp <kontakt@rpiton.com>
2021-11-20 22:22:59 +0100

    WIP buildable image

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,11 @@ module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-actions/register",
+    "@storybook/addon-knobs/register",
     "@storybook/addon-links/register",
-    "@storybook/addon-controls/register",
-    "@storybook/addon-viewport/register",
+    "@storybook/addon-viewport/register"
   ],
-  presets: ["@storybook/preset-create-react-app"],
+  presets: [
+    "@storybook/preset-create-react-app"
+  ],
 }
