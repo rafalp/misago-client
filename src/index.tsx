6878e485330b73f1ffe79aa7fb194c36db5d2d89
rafalp <kontakt@rpiton.com>
2021-11-28 02:22:48 +0100

    Fix default index.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { ApolloProvider } from "@apollo/client"
 import ReactDOM from "react-dom"
 import App from "./App"
 import { client } from "./apolloClient"
-import setupPlugins from "."
+import setupPlugins from "./plugins"
 import * as serviceWorker from "./serviceWorker"
 import "./styles/index.scss"
 
