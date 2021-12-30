6bb80003c32e14a8250f21aa5560bef17945b969
rafalp <kontakt@rpiton.com>
2021-12-30 16:22:39 +0100

    Make prod builds put assets under misago namespace

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,52 @@
 const CracoAlias = require("craco-alias")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-   plugins: [
-     {
-        plugin: CracoAlias,
-        options: {
-           source: "tsconfig",
-           baseUrl: "./src/",
-           tsConfigPath: "./tsconfig.paths.json"
-        }
-     }
-  ]
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      if (webpackConfig["mode"] !== "production") return webpackConfig
+
+      // Namespace js files under "misago"
+      webpackConfig.output.filename =
+        "static/misago/js/[name].[contenthash:8].js"
+      webpackConfig.output.chunkFilename =
+        "static/misago/js/[name].[contenthash:8].chunk.js"
+
+      // Same with media files
+      webpackConfig.module.rules.forEach((ruleset) => {
+        if (!ruleset.oneOf) return
+
+        ruleset.oneOf.forEach((rule) => {
+          if (rule.options && rule.options.name) {
+            rule.options.name = rule.options.name.replace(
+              "static",
+              "static/misago"
+            )
+          }
+        })
+      })
+
+      // And CSS files
+      webpackConfig.plugins.forEach((plugin) => {
+        if (!(plugin instanceof MiniCssExtractPlugin)) return
+
+        plugin.options.filename =
+          "static/misago/css/[name].[contenthash:8].css"
+        plugin.options.chunkFilename =
+          "static/misago/css/[name].[contenthash:8].chunk.css"
+      })
+
+      return webpackConfig
+    },
+  },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        baseUrl: "./src/",
+        tsConfigPath: "./tsconfig.paths.json",
+      },
+    },
+  ],
 }
