45d5892e69d7aa6916289b18ea5ed94e88fbd9f5
rafalp <kontakt@rpiton.com>
2022-02-17 22:17:32 +0100

    Tweak categories list colors more

--- src/UI/CategoriesNav/CategoriesNav.tsx
+++ src/UI/CategoriesNav/CategoriesNav.tsx
@@ -18,7 +18,7 @@ const CategoriesNav: React.FC<CategoriesNavProps> = ({ active }) => {
   return (
     <SideNav className="categories-nav">
       <SideNavItem
-        icon={<CategoryIcon className="nav-link-icon" />}
+        icon={<CategoryIcon category={{ icon: "fas fa-list", color: null }} />}
         to={settings.forumIndexThreads ? urls.index() : urls.threads()}
         isActive={!active}
       >
